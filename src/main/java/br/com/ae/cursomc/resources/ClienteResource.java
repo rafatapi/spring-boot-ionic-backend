c34b27d299765edb7565981e2bc37f752bc56490
Rafael Tapia <rafatapi@gmail.com>
2019-08-27 09:35:06 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/br/com/ae/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/ae/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.ae.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.ae.cursomc.domain.Categoria;
 import br.com.ae.cursomc.domain.Cliente;
 import br.com.ae.cursomc.dto.ClienteDTO;
+import br.com.ae.cursomc.dto.ClienteNewDTO;
 import br.com.ae.cursomc.services.ClienteService;
 
 @RestController
@@ -32,6 +36,16 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	// -----------------------------------------------------------------------
+	// INSERT - POST
+	// -----------------------------------------------------------------------
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Categoria> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}	
 
 	// -----------------------------------------------------------------------
 	// UPDATE - PUT
--- src/main/java/br/com/ae/cursomc/services/ClienteService.java
+++ src/main/java/br/com/ae/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.ae.cursomc.domain.Cidade;
 import br.com.ae.cursomc.domain.Cliente;
+import br.com.ae.cursomc.domain.Endereco;
+import br.com.ae.cursomc.domain.enums.TipoCliente;
 import br.com.ae.cursomc.dto.ClienteDTO;
+import br.com.ae.cursomc.dto.ClienteNewDTO;
 import br.com.ae.cursomc.repositories.ClienteRepository;
+import br.com.ae.cursomc.repositories.EnderecoRepository;
 import br.com.ae.cursomc.services.exceptions.DataIntegrityException;
 import br.com.ae.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired 
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -28,6 +37,13 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -43,24 +59,40 @@ public class ClienteService {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
 		}
 	}
-	
-	public List<Cliente> findAll(){
+
+	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(),null,null);
-	}	
-	
-	private void updateData(Cliente newObj, Cliente obj) {
-		newObj.setNome( obj.getNome() );
-		newObj.setEmail(obj.getEmail());
-		
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 }
